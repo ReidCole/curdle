b3ddb0ae04d2253ae074cc25702ad4fda1152c56
Reid Cole <reid-cole@outlook.com>
2025-01-24 22:29:37 -0500

    yeah

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 const url = Deno.args[0];
 const res = await fetch(url);
-if (res.status >= 400) {
+if (res.status >= 400 && res.status !== 429) {
   console.error(`FETCH FAILED WITH STATUS: ${res.status} - ${url}
 
 SERVER RESPONSE:
